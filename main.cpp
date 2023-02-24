8d55892d5f7a5c49501e30d08a1746ef6df6b769
Vaidas-code <116501117+Vaidas-code@users.noreply.github.com>
2023-02-24 03:13:29 +0200

    masyvo varianto failas

--- main.cpp
+++ main.cpp
@@ -1,239 +1,135 @@
 #include "Mylib.h"
 
 struct studentas {
-	string vardas, pavarde;
-	int egz=0;
-	vector<int> paz;
+    string vardas, pavarde;
+    int egz;
+    int* paz;
+    double vid, med;
+    bool random = false;
 };
 
-// liepia ivesti varda, pavarde, egzamino ir namu darbu rezultatus
-void pild(studentas& tempas) {
-	cout << "Iveskite varda ir pavarde: ";
-	cin >> tempas.vardas >> tempas.pavarde;
-	int x;
-	cout << "Iveskite pazymius, kai noresite baigti, parasykite raide arba skaiciu didesni uz 10: ";
-	cin >> x;
-	while (cin.fail() || x <= 0 || x > 10)
-	{
-		cin.clear();
-		cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		cout << "Skaicius privalo buti nuo 1-10" << endl;
-		cin >> x;
-		tempas.paz.push_back(x);
-	}
-	cin >> x;
-	while (cin.fail() || x < 0 || x > 10)
-	{
-		cin.clear();
-		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		cout << "Skaicius privalo buti nuo 1-10" << endl;
-		cin >> x;
-		tempas.paz.push_back(x);
-	}
-	while (cin >> x )
-	{
-		if (x <= 0 || x>10 )
-		{
-			break;
-		}
-		tempas.paz.push_back(x);
-	}
-	cin.clear();
-	cin.ignore(numeric_limits<streamsize>::max(), '\n');
-	cout << "Iveskite egzamino paz: ";
-	cin >> tempas.egz;
-	while (cin.fail() || tempas.egz < 0 || tempas.egz > 10) {
-		cout << "Iveskite skaiciu nuo 1-10" << endl;
-		cin.clear();
-		cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		cin >> tempas.egz;
-	}
-	random_device rd;
-	mt19937 gen(rd());
-	uniform_int_distribution<int> dist(1, 10);
-	cout << "Ar norite kad skaiciai butu generuojami? Jei taip, irasykite '1'," << endl;
-	cout << "jei ne, bet koki kita simboli ar skaiciu" << endl;
-	int uzklausa;
-	cin >> uzklausa;
-	if (uzklausa == 1)
-	{
-		for (int i = 0; i < tempas.paz.size(); i++)
-			tempas.paz[i] = dist(gen);
-		tempas.egz = dist(gen);
-	}
-}
-// apskaiciuoja vidurki
-double vidurkis(const vector<int>& paz, studentas& tempas) {
-	double sum = accumulate(paz.begin(), paz.end(), 0);
-	if (paz.size() > 0)
-		return 0.4 * (sum / paz.size()) + 0.6 * tempas.egz;
-	else
-		return 0;
-}
-
-// apskaiciuoja mediana
-double mediana(vector<int>& paz, studentas& tempas) {
-	if (paz.size() != 0)
-	{
-		int n = paz.size();
-		sort(paz.begin(), paz.end());
-		if (n % 2 == 0) {
-			return (double)0.4 * ((paz[n / 2 - 1] + paz[n / 2]) / 2) + 0.6 * tempas.egz;
-		}
-		else {
-			return (double)0.4 * (paz[n / 2]) + 0.6 * tempas.egz;
-		}
-	}
-	else
-		return 0;
-}
-// ispausdina visus reikalingus duomenis
-void spausd(studentas& tempas) {
-	cout << left << setw(10) << tempas.vardas << setw(15) << tempas.pavarde;
-	cout << setw(12) << fixed << setprecision(2) << vidurkis(tempas.paz, tempas);
-	cout  << setw(10) << fixed << setprecision(2) << mediana(tempas.paz, tempas) << endl;
-}
-int main() {
-	vector <studentas> mas;
-	studentas tempasas;
-	char uzkl = 'n';
-		do {
-			pild(tempasas);
-			mas.push_back(tempasas);
-			tempasas.paz.clear();
-			cout << "Baigti darba spausk n, testi - bet koks klavisas: ";
-			cin >> uzkl;
-			if (uzkl == 'n' || uzkl == 'N')
-			{
-				cout << left << setw(10) << "Vardas" << setw(10) << "Pavarde" << setw(20) << "Galutinis (Vid.) / Galutinis (Med.)" << endl;
-				cout << "-----------------------------------------------------------" << endl;
-			}
-
-		} while (uzkl != 'n' && uzkl != 'N');
-		{
-			for (auto& i : mas) spausd(i);
-			for (auto& i : mas) i.paz.clear();
-		}
-
-	}
-
-	#include "Mylib.h"
-
-struct studentas {
-	string vardas, pavarde;
-	int egz=0;
-	vector<int> paz;
+struct studentai {
+    studentas* studentas;
+    int dydis;
 };
 
-// liepia ivesti varda, pavarde, egzamino ir namu darbu rezultatus
-void pild(studentas& tempas) {
-	cout << "Iveskite varda ir pavarde: ";
-	cin >> tempas.vardas >> tempas.pavarde;
-	int x;
-	cout << "Iveskite pazymius, kai noresite baigti, parasykite raide arba skaiciu didesni uz 10: ";
-	cin >> x;
-	while (cin.fail() || x <= 0 || x > 10)
-	{
-		cin.clear();
-		cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		cout << "Skaicius privalo buti nuo 1-10" << endl;
-		cin >> x;
-		tempas.paz.push_back(x);
-	}
-	cin >> x;
-	while (cin.fail() || x < 0 || x > 10)
-	{
-		cin.clear();
-		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		cout << "Skaicius privalo buti nuo 1-10" << endl;
-		cin >> x;
-		tempas.paz.push_back(x);
-	}
-	while (cin >> x )
-	{
-		if (x <= 0 || x>10 )
-		{
-			break;
-		}
-		tempas.paz.push_back(x);
-	}
-	cin.clear();
-	cin.ignore(numeric_limits<streamsize>::max(), '\n');
-	cout << "Iveskite egzamino paz: ";
-	cin >> tempas.egz;
-	while (cin.fail() || tempas.egz < 0 || tempas.egz > 10) {
-		cout << "Iveskite skaiciu nuo 1-10" << endl;
-		cin.clear();
-		cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		cin >> tempas.egz;
-	}
-	random_device rd;
-	mt19937 gen(rd());
-	uniform_int_distribution<int> dist(1, 10);
-	cout << "Ar norite kad skaiciai butu generuojami? Jei taip, irasykite '1'," << endl;
-	cout << "jei ne, bet koki kita simboli ar skaiciu" << endl;
-	int uzklausa;
-	cin >> uzklausa;
-	if (uzklausa == 1)
-	{
-		for (int i = 0; i < tempas.paz.size(); i++)
-			tempas.paz[i] = dist(gen);
-		tempas.egz = dist(gen);
-	}
-}
-// apskaiciuoja vidurki
-double vidurkis(const vector<int>& paz, studentas& tempas) {
-	double sum = accumulate(paz.begin(), paz.end(), 0);
-	if (paz.size() > 0)
-		return 0.4 * (sum / paz.size()) + 0.6 * tempas.egz;
-	else
-		return 0;
+void mediana(studentas* tempas, int dydis) {
+    {
+        sort(tempas->paz, tempas->paz + dydis, greater<int>());
+    }
+    int vidurys = dydis / 2;
+    float mediana = dydis % 2 == 0 ? (tempas->paz[vidurys - 1] + tempas->paz[vidurys]) / 2.0 : tempas->paz[vidurys];
+    tempas->med = 0.4 * mediana + 0.6 * tempas->egz;
 }
 
-// apskaiciuoja mediana
-double mediana(vector<int>& paz, studentas& tempas) {
-	if (paz.size() != 0)
-	{
-		int n = paz.size();
-		sort(paz.begin(), paz.end());
-		if (n % 2 == 0) {
-			return (double)0.4 * ((paz[n / 2 - 1] + paz[n / 2]) / 2) + 0.6 * tempas.egz;
-		}
-		else {
-			return (double)0.4 * (paz[n / 2]) + 0.6 * tempas.egz;
-		}
-	}
-	else
-		return 0;
+void keistiint(int*& array, int dydis) {
+    int* newArray = new int[dydis];
+    for (int i = 0;i < dydis - 1;i++) {
+        newArray[i] = array[i];
+    }
+    delete[] array;
+    array = newArray;
 }
-// ispausdina visus reikalingus duomenis
-void spausd(studentas& tempas) {
-	cout << left << setw(10) << tempas.vardas << setw(15) << tempas.pavarde;
-	cout << setw(12) << fixed << setprecision(2) << vidurkis(tempas.paz, tempas);
-	cout  << setw(10) << fixed << setprecision(2) << mediana(tempas.paz, tempas) << endl;
+//liepia ivesti varda, pavarde, egzamino ir namu darbu rezultatus
+void pild(studentas* tempas) {
+    int uzkl;
+    cout << "Iveskite varda ir pavarde: ";
+    cin >> tempas->vardas >> tempas->pavarde;
+    double x, suma = 0;
+    int n = 0;
+    tempas->paz = NULL;
+    cout << "Iveskite pazymius, kai noresite baigti, parasykite raide arba skaiciu didesni uz 10: " << endl;
+    cin >> x;
+    while (cin.fail() || x <= 0 || x > 10)
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Skaicius privalo buti nuo 1-10" << endl;
+        cin >> x;
+    }
+    tempas->paz = new int[1];
+    tempas->paz[0] = x;
+    suma += x;
+    n++;
+    while (cin >> x && x > 0 && x<=10) {
+            suma += x;
+            n++;
+            keistiint(tempas->paz, n);
+            tempas->paz[n - 1] = x;
+    }
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    cout << "Iveskite egzamino paz: " << endl;
+    cin >> tempas->egz;
+    
+    while (tempas->egz < 1 || tempas->egz > 10 || cin.fail())
+    {
+            cout << "Iveskite skaiciu nuo 1-10" << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cin >> tempas->egz;
+    }
+    cout << "Ar norite kad skaiciai butu generuojami? Jei taip, irasykite '1'" << endl;
+    cin >> uzkl;
+    if (uzkl == 1) {
+        srand(time(NULL));
+        tempas->vid = (double)rand() / RAND_MAX * 10;
+        tempas->med = (double)rand() / RAND_MAX * 10;
+    }
+    else if (n > 0) {
+        tempas->vid = 0.4 * (suma / n) + 0.6 * tempas->egz;
+        mediana(tempas, n);
+    }
 }
+// studentu masyvo keitimas
+void keististruk(studentas*& array, int dydis) {
+    studentas* newArray = new studentas[dydis];
+    for (int i = 0;i < dydis - 1;i++) {
+        newArray[i] = array[i];
+    }
+    delete[] array;
+    array = newArray;
+}
+// atspausdinimas duomenu
+void spausd(studentas* tempas) {
+    cout << setw(10) << left << tempas->vardas << setw(15) << left << tempas->pavarde;
+    cout << setw(17) << left << setprecision(2) << fixed << tempas->vid << setw(3) << setprecision(2) << fixed << tempas->med;
+    cout << endl;
+}
+
 int main() {
-	vector <studentas> mas;
-	studentas tempasas;
-	char uzkl = 'n';
-		do {
-			pild(tempasas);
-			mas.push_back(tempasas);
-			tempasas.paz.clear();
-			cout << "Baigti darba spausk n, testi - bet koks klavisas: ";
-			cin >> uzkl;
-			if (uzkl == 'n' || uzkl == 'N')
-			{
-				cout << left << setw(10) << "Vardas" << setw(10) << "Pavarde" << setw(20) << "Galutinis (Vid.) / Galutinis (Med.)" << endl;
-				cout << "-----------------------------------------------------------" << endl;
-			}
+    int n = 0;
+    char uzkl=' ';
+    studentai Kursas;
 
-		} while (uzkl != 'n' && uzkl != 'N');
-		{
-			for (auto& i : mas) spausd(i);
-			for (auto& i : mas) i.paz.clear();
-		}
+    while (uzkl != 'n' && uzkl != 'N') {
+        if (n == 0) { //pati pirma karta bus inicijuojamas masyvas
+            Kursas.studentas = new studentas[1]; //duodame tik viena vieta
+            ++n; //max tampa 1, kad loopas vel nesoktu i if{}
+            Kursas.dydis = n;
+            pild(&Kursas.studentas[0]); //uzpildome ta viena vieta
+        }
+        else { //visais kitais kartais
+            ++n; //padidiname skaitliuka, (antra karta bus 2)
+            keististruk(Kursas.studentas, n); //padidiname masyva
+            pild(&Kursas.studentas[n - 1]); //uzpildome ta suteikta vieta
+        }
+        if (Kursas.studentas[n - 1].paz == NULL) { //leidzia viena karta pabandyti is naujo
+            cout << "Neivedete jokiu studentu duomenu, iveskite is nujo." << endl;
+            pild(&Kursas.studentas[n - 1]);
+        }
+        Kursas.dydis = n;
+        cout << "Baigti darba? Spausk n, testi - bet koks kitas klavisas" << endl;
+        cin >> uzkl;
+    }
 
-	}
-
-	
+    cout << endl << endl << setw(10) << left << "Vardas" << setw(15) << left << "Pavarde" << setw(17) << "Galutinis (Vid.) " << setw(18) << "/ Galutinis (Med.)";
+    cout << endl << "------------------------------------------------------------" << endl;
+    for (int i = 0;i < Kursas.dydis;i++) {
+        spausd(&Kursas.studentas[i]);
+        delete[] Kursas.studentas[i].paz;
+    }
+    cout << endl;
+    delete[] Kursas.studentas;
+}
