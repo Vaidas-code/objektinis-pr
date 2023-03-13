ed06edeb6045d3a2ebf7fb656d367490e271eb4a
Vaidas-code <116501117+Vaidas-code@users.noreply.github.com>
2023-03-13 23:58:43 +0200

    Add files via upload

--- functions.cpp
+++ functions.cpp
@@ -1,11 +1,11 @@
 #include "Mylib.h"
-const char FILENAME[] = "kursiokai.txt";
+const char FILENAME[] = "kursiokai1.txt";
 
 struct studentas {
 	string vardas, pavarde;
 	int egz = 0;
 	vector<int> paz;
-
+	double vid, med;
 };
 //palyginam vardus
 bool palyginti_vardus(const studentas& a, const studentas& b);
@@ -14,3 +14,5 @@ void pild(studentas& tempas);
 double vidurkis(const vector<int>& paz, studentas& tempas);
 double mediana(vector<int>& paz, studentas& tempas);
 void spausd(studentas& tempas);
+void sukurti1(studentas& tempas, vector<studentas>& mas);
+bool palyginti_galutinius(const studentas& a, const studentas& b);
