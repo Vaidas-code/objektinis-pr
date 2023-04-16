ae69317983dae4c9e817fc5d87a19960f48619b2
Vaidas-code <116501117+Vaidas-code@users.noreply.github.com>
2023-04-16 15:45:43 +0300

    Add files via upload

---
+++ deque.func.cpp
@@ -0,0 +1,19 @@
+#include "Deque.h"
+#define _CRT_SECURE_NO_WARNINGS
+struct studentas {
+	string vardas, pavarde;
+	int egz = 0;
+	deque<int> paz;
+	double vid, med;
+};
+//palyginam vardus
+bool palyginti_vardus(const studentas& a, const studentas& b);
+void skait(studentas& tempas, int kiekis, deque<studentas>& mas, string pav);
+void pild(studentas& tempas);
+double vidurkis(const deque<int>& paz, studentas& tempas);
+double mediana(deque<int>& paz, studentas& tempas);
+void spausd(studentas& tempas);
+void sukurti1(studentas& tempas, deque<studentas>& mas);
+bool palyginti_galutinius(const studentas& a, const studentas& b);
+void fgeneravimas(int kiekis, string failovardas, double& laikas, bool genl);
+bool egzistuojantis(const string& filename);
